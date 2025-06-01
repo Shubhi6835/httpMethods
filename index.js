acd401a0b35f8b722fc2ee4fe0074d4d5d8e3592
Shubhi6835 <shubhr68350@gmail.com>
2025-06-01 23:04:00 +0530

    Learning about HTTP Methods

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.send("Hello World");
+  });
+
+  app.get("/about", (req, res)=> {
+    res.send("<h1>This is the about page</h1>");
+  });
+
+app.listen(3000, () =>{
+ console.log("Server is up and running");
+});
